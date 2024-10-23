2dbf68d85efb99dc9752bf1becd475cb6dc2fb1a
EvgLav <www.coldreks@gmail.com>
2024-10-23 14:48:49 +0300

    add

--- src/main/java/com/sorting_app/Main.java
+++ src/main/java/com/sorting_app/Main.java
@@ -1,17 +1,24 @@
 package com.sorting_app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.sorting_app.input.ItemInputManager;
+import com.sorting_app.list.ClassesList;
+import com.sorting_app.list.WriterCSV;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        ClassesList classesList = new ClassesList();
+        ItemInputManager itemInputManager = new ItemInputManager(classesList);
+        WriterCSV writerCSV = new WriterCSV();
+
+        itemInputManager.addItem();
+        itemInputManager.addItem();
+
+        writerCSV.writeCars(classesList.getCarList(), "CARS.csv");
+        classesList.printCars();
+
     }
 }
