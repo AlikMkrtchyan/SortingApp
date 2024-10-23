b9cc4d240ddca8d6512aae5555ea4914fe8abcbd
Daniil Migurin <ubrandnw@gmail.com>
2024-10-24 00:30:33 +0300

    Utils add Sort

---
+++ src/main/java/com/sorting_app/utils/Sort.java
@@ -0,0 +1,33 @@
+package com.sorting_app.utils;
+
+import com.sorting_app.data.DataObject;
+import com.sorting_app.strategy.IStrategy;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Sort implements IStrategy {
+    private DataObject sortedData;
+
+    public Sort(DataObject dataToSort) {
+        this.sortedData = dataToSort;
+    }
+
+    private void sort() {
+        if (sortedData.cars != null) {
+            MergeSort.mergeSort(sortedData.cars);
+        }
+        if (sortedData.books != null) {
+            MergeSort.mergeSort(sortedData.books);
+        }
+        if (sortedData.rootVegetables != null) {
+            MergeSort.mergeSort(sortedData.rootVegetables);
+        }
+    }
+
+    @Override
+    public Objects getResult(List<Objects> obj) {
+        this.sort();
+        return null;
+    }
+}
