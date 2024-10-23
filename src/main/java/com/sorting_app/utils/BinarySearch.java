fdbd66a1ef2e2f654b8633265fdc48644847fb55
Daniil Migurin <ubrandnw@gmail.com>
2024-10-23 13:37:36 +0300

    Utils commit add MergeSort, BinarySearch, CustomSearch

---
+++ src/main/java/com/sorting_app/utils/BinarySearch.java
@@ -0,0 +1,24 @@
+package com.sorting_app.utils;
+
+import java.util.List;
+
+class BinarySearch {
+    public static <T extends Comparable<T>> int binarySearch(List<T> list, T key) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int cmp = list.get(mid).compareTo(key);
+
+            if (cmp == 0) {
+                return mid;
+            } else if (cmp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/sorting_app/utils/CustomSort.java
@@ -0,0 +1,5 @@
+package com.sorting_app.utils;
+
+public class CustomSort {
+    //In development...
+}
