202dc285488d565e129a114d27e920ff6cd2f7ce
Aleks <grulpud@yandex.ru>
2024-10-23 23:45:43 +0300

    add exception handler

---
+++ src/main/java/com/sorting_app/handler/ExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.sorting_app.handler;
+
+import com.sorting_app.model.Car;
+
+public class ExceptionHandler {
+
+    public static String handlerCar(Car car){
+        return "";
+
+    }
+}
--- src/main/java/com/sorting_app/validation/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.sorting_app.validation;
-
-public class ValidationException extends Exception {
-
-
-
-}
