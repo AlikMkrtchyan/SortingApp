784565c04fcefb0be16b4f1f4b87ae47024958d1
Ilya Kostylev <ilya.kostylev.1997@mail.ru>
2024-10-26 15:11:52 +0300

    change Context, Printer. add sort, find, record in ExecutionCycle

--- src/main/java/com/sorting_app/ExecutionCycle/Context.java
+++ src/main/java/com/sorting_app/ExecutionCycle/Context.java
@@ -1,15 +1,9 @@
 package com.sorting_app.ExecutionCycle;
 
-import com.sorting_app.data.DataObject;
 import com.sorting_app.strategy.IStrategy;
 
 public class Context {
-    //private DataObject dataObject;
     private IStrategy strategy;
-
-    //public Context(DataObject dataObject) {
-    //    this.dataObject = dataObject;
-    //}
 
     public void setStrategy(IStrategy strategy) {
         this.strategy = strategy;
