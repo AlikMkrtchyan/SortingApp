8ef1f9eea1891fc1cb16d271c35cc13a2cfc2c9c
Ilya Kostylev <ilya.kostylev.1997@mail.ru>
2024-10-26 15:11:52 +0300

    change Context, Printer. add sort, find, record in ExecutionCycle

--- src/main/java/com/sorting_app/ExecutionCycle/ExecutionCycle.java
+++ src/main/java/com/sorting_app/ExecutionCycle/ExecutionCycle.java
@@ -1,11 +1,8 @@
 package com.sorting_app.ExecutionCycle;
 
-import com.sorting_app.Utils.Printer;
+import com.sorting_app.utils.Printer;
 import com.sorting_app.data.DataObject;
-import com.sorting_app.strategy.InputConsoleStrategy;
-import com.sorting_app.strategy.InputFileStrategy;
-import com.sorting_app.strategy.InputRandomStrategy;
-import com.sorting_app.strategy.PrintObjectStrategy;
+import com.sorting_app.strategy.*;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -41,19 +38,19 @@ public class ExecutionCycle {
                     findElement();
                     break;
                 }
-//                case 4: {
-//                    writeSortedCollectionInFile();
-//                    break;
-//                }
+                case 4: {
+                    recordCollectionInFile();
+                    break;
+                }
 //                case 5: {
 //                    writeFoundElementsInFile();
 //                    break;
 //                }
-                case 4: {
+                case 5: {
                     printCollection();
                     break;
                 }
-                case 5: {
+                case 6: {
                     isExit = true;
                     break;
                 }
@@ -101,21 +98,51 @@ public class ExecutionCycle {
         }
     }
 
-    public static void sortCollection() {
+    private void sortCollection() {
+        isCorrect = false;
 
+        System.out.println("Ведите номер желаемого способа сортировки коллекции:" +
+                "\n1 - mergeSort;" +
+                "\n2 - сортировка по числовому полю.\n");
+
+        while (!isCorrect) {
+            enterCommand();
+            switch (command) {
+                case 1: {
+                    context.setStrategy(new MergeSortStrategy(dataObject));
+                    context.commandSelected();
+                    isCorrect = true;
+                    break;
+                }
+                case 2: {
+//                    context.setStrategy(new (dataObject));
+//                    context.commandSelected();
+                    isCorrect = true;
+                    break;
+                }
+                default: {
+                    System.out.println("Введите число от 1 до 2.");
+                }
+            }
+        }
     }
 
-    public static void findElement() {
-        boolean isFound = false;
+    public void findElement() {
+//        boolean isFound = false;
+//
+        context.setStrategy(new BinarySearchStrategy(dataObject));// find
+        context.commandSelected();
+//        if (isFound) {
+//            System.out.println("Элемент найден");
+//            //writeFoundElementInFile();
+//        } else {
+//            System.out.println("Элемент не найден");
+//        }
+    }
 
-        // find
-
-        if (isFound) {
-            System.out.println("Элемент найден");
-            //writeFoundElementInFile();
-        } else {
-            System.out.println("Элемент не найден");
-        }
+    private void recordCollectionInFile() {
+        context.setStrategy(new RecordStrategy(dataObject));
+        context.commandSelected();
     }
 
     private void printCollection() {
