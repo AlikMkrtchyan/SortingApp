b83564880c88f83ddbbcb4f32b1f8e2f758f78a8
Aleks <grulpud@yandex.ru>
2024-10-26 23:46:26 +0300

    refactoring project - text out

--- src/main/java/com/sorting_app/Main.java
+++ src/main/java/com/sorting_app/Main.java
@@ -1,5 +1,6 @@
 package com.sorting_app;
 
+import com.sorting_app.ExecutionCycle.ExecutionCycle;
 import com.sorting_app.data.DataObject;
 import com.sorting_app.strategy.*;
 
@@ -9,19 +10,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         DataObject dataObject = new DataObject();
-
-        InputConsoleStrategy inputConsoleStrategy = new InputConsoleStrategy(dataObject);
-        InputRandomStrategy inputRandomStrategy = new InputRandomStrategy(dataObject);
-        InputFileStrategy inputFileStrategy = new InputFileStrategy(dataObject);
-        PrintObjectStrategy printObjectStrategy = new PrintObjectStrategy(dataObject);
-        RecordStrategy record = new RecordStrategy(dataObject);
-        BinarySearchStrategy search = new BinarySearchStrategy(dataObject);
-        SortByEvenStrategy sortByEvenStrategy = new SortByEvenStrategy(dataObject);
-        MergeSortStrategy mergeSortStrategy = new MergeSortStrategy(dataObject);
-
-        inputFileStrategy.getResult();
-        search.getResult();
-
+        ExecutionCycle executionCycle = new ExecutionCycle(dataObject);
+        executionCycle.run();
 
     }
 }
